b4961f846e561d55162323478e43825e85584614
Jay Faulkner <jay@jvf.cc>
2014-06-25 16:51:09 -0700

    Add infra build script to IPA
    We now do builds of the IPA CoreOS ramdisk in the post script. (Ref:
    openstack-infra/config/tree/modules/openstack_project/files/
    jenkins_job_builder/config/ironic-python-agent-jobs.yaml)
    We should store the build script for this in the local repo so we can
    modify it as needed without needing commits to openstack-infra/config.
    
    Change-Id: I92655c0552179613c23b129d1e2dce2ef1643391

---
+++ imagebuild/coreos/full_trusty_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -xe
+#
+# From a base-trusty node, this should build a CoreOS IPA image
+# suitable for use in testing or production.
+#
+sudo apt-get update
+sudo apt-get install -y docker.io
+sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
+sudo sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
+cd imagebuild/coreos/
+make
+tar czf ../../ipa-coreos.tar.gz UPLOAD/coreos_production_pxe_image-oem.cpio.gz UPLOAD/coreos_production_pxe.vmlinuz
