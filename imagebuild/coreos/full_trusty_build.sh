cf99515f9785937d6779fc148a6aa22d1c794058
Jim Rollenhagen <jim@jimrollenhagen.com>
2014-07-02 11:04:00 -0700

    Add current directory logging to build script
    Helps debug post job.
    
    Change-Id: Ia30a8b5511dc087ddd1ecdbfe8eb3ca1e63a0ad5

--- imagebuild/coreos/full_trusty_build.sh
+++ imagebuild/coreos/full_trusty_build.sh
@@ -8,5 +8,6 @@ sudo apt-get install -y docker.io
 sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
 sudo sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
 cd imagebuild/coreos/
+pwd
 make
 tar czf ../../ipa-coreos.tar.gz UPLOAD/coreos_production_pxe_image-oem.cpio.gz UPLOAD/coreos_production_pxe.vmlinuz
