9f855a0bcfa0b7f83435d1029d4d7967bdefcff9
Ramakrishnan G <rameshg87@gmail.com>
2014-09-19 10:43:25 +0530

    Preserve environment variables when doing sudo
    This commit adds the '-E' option for all the sudo commands
    in build script of agent which causes it to preserve environment
    variables.
    
    Change-Id: Ic3fbc4ee8189cc82ef99359bf61eef33dae40a5a
    Closes-Bug: 1371418

--- imagebuild/coreos/build_coreos_image.sh
+++ imagebuild/coreos/build_coreos_image.sh
@@ -16,9 +16,9 @@
 # all requirements then perform the build.
 
 if [[ -x /usr/bin/docker.io ]]; then
-    sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
+    sudo -E ln -sf /usr/bin/docker.io /usr/local/bin/docker
 fi
 cd imagebuild/coreos
-sudo pip install -r requirements.txt
-sudo make clean
-sudo make
+sudo -E pip install -r requirements.txt
+sudo -E make clean
+sudo -E make
