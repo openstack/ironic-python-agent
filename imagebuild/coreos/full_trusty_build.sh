be16b148117e4841ea1abb9fe364d5538535de9c
Lucas Alvares Gomes <lucasagomes@gmail.com>
2016-03-24 12:00:54 +0000

    CoreOS image building: Add dnf support
    Yum is not the default packager manager for fedora anymore and may be
    removed in the future.
    
    Change-Id: I9fca730b9e73e4950485edbf3303954b94fed74e

--- imagebuild/coreos/full_trusty_build.sh
+++ imagebuild/coreos/full_trusty_build.sh
@@ -3,15 +3,21 @@
 # From a base-trusty node, this should build a CoreOS IPA image
 # suitable for use in testing or production.
 #
+
+# NOTE(lucasagomes): List of dependencies for Red Hat systems
+REDHAT_PACKAGES="docker-io gpg"
+
 if [ -x "/usr/bin/apt-get" ]; then
     sudo -E apt-get update
     # apparmor is an undeclared dependency for docker on ubuntu
     # https://github.com/docker/docker/issues/9745
     sudo -E apt-get install -y docker.io apparmor
+elif [ -x "/usr/bin/dnf" ]; then
+    sudo -E dnf install -y $REDHAT_PACKAGES
 elif [ -x "/usr/bin/yum" ]; then
-    sudo -E yum install -y docker-io gpg
+    sudo -E yum install -y $REDHAT_PACKAGES
 else
-    echo "No supported package manager installed on system. Supported: apt, yum"
+    echo "No supported package manager installed on system. Supported: apt, yum, dnf"
     exit 1
 fi
 
