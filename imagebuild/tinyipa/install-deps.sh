06183752b51a6a860c813006cf13b227588d1575
Markos Chandras <mchandras@suse.de>
2017-02-03 11:34:35 +0000

    imagebuild: tinyipa: Make the install-deps.sh SUSE aware
    Add zypper support in the install-deps.sh script so tinyipa can be
    built on SUSE distributions.
    
    Change-Id: Ie7ac36a9cab3dba825357ccd1e58290873335a68

--- imagebuild/tinyipa/install-deps.sh
+++ imagebuild/tinyipa/install-deps.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-PACKAGES="wget python-pip unzip sudo gawk squashfs-tools"
+COMMON_PACKAGES="wget python-pip unzip sudo gawk"
+APT_PACKAGES="${COMMON_PACKAGES} squashfs-tools"
+YUM_PACKAGES="${COMMON_PACKAGES} squashfs-tools"
+ZYPPER_PACKAGES="${COMMON_PACKAGES} squashfs"
 
 echo "Installing dependencies:"
 
-if [ -x "/usr/bin/apt-get" ]; then
+# first zypper in case zypper-aptitude is installed
+if [ -x "/usr/bin/zypper" ]; then
+    sudo -E zypper -n install -l ${ZYPPER_PACKAGES}
+elif [ -x "/usr/bin/apt-get" ]; then
     sudo -E apt-get update
-    sudo -E apt-get install -y $PACKAGES
+    sudo -E apt-get install -y ${APT_PACKAGES}
 elif [ -x "/usr/bin/dnf" ]; then
-    sudo -E dnf install -y $PACKAGES
+    sudo -E dnf install -y ${YUM_PACKAGES}
 elif [ -x "/usr/bin/yum" ]; then
-    sudo -E yum install -y $PACKAGES
+    sudo -E yum install -y ${YUM_PACKAGES}
 else
-    echo "No supported package manager installed on system. Supported: apt, yum, dnf"
+    echo "No supported package manager installed on system. Supported: apt, yum, dnf, zypper"
     exit 1
 fi
