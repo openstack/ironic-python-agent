14e7ad3f76caaaeac73b7598f5bcee2a4dc6347e
Dmitry Tantsur <dtantsur@protonmail.com>
2019-09-25 11:47:23 +0200

    Fix compatibility with Pint 0.5
    Conversion from Quantity to int/long was added only in 0.5.2.
    
    Change-Id: I23f93d29a0624b77f4624c6af530971eb6a4c01b
    (cherry picked from commit 386654f602c86495515811dd741652a3b0292fd3)

--- ironic_python_agent/numa_inspector.py
+++ ironic_python_agent/numa_inspector.py
@@ -82,7 +82,7 @@ def get_nodes_memory_info(numa_node_dirs):
             # To get memory size with unit from memory info line
             # Memory info sample line format 'Node 0 MemTotal: 1560000 kB'
             value = line.split(":")[1].strip()
-            memory_kb = int(UNIT_CONVERTER(value).to_base_units())
+            memory_kb = int(UNIT_CONVERTER(value).to_base_units().magnitude)
         except (ValueError, IndexError, pint.UndefinedUnitError) as exc:
             msg = ('Failed to get memory information for %(node)s: '
                    '%(error)s' % {'node': numa_node_dir, 'error': exc})
