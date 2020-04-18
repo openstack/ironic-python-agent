5589b05df43b35cf75ddf4092fd14379b673f58a
Sean McGinnis <sean.mcginnis@gmail.com>
2020-04-18 11:53:28 -0500

    Use unittest.mock instead of third party mock
    Now that we no longer support py27, we can use the standard library
    unittest.mock module instead of the third party mock lib.
    
    Change-Id: I5fdb2a02ee83c692d46cbe28266fcae033bec6f6
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>

--- ironic_python_agent/tests/unit/extensions/test_deploy.py
+++ ironic_python_agent/tests/unit/extensions/test_deploy.py
@@ -10,7 +10,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import mock
+from unittest import mock
 
 from ironic_python_agent import errors
 from ironic_python_agent.extensions import deploy
